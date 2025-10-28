914cc885a24cfc781adbb4591a960849e947a22d
Hendy Nur Sholeh <hendynursholeh7213@gmail.com>
2025-10-28 15:08:57 +0800

    chore: change view log akselgate modal

--- app/Views/log/akselgate/_detail_modal.blade.php
+++ app/Views/log/akselgate/_detail_modal.blade.php
@@ -1,9 +1,9 @@
-<!-- Modal Detail Log Transaksi -->
+<!-- Modal Detail Aksel Gate Response Log -->
 <div class="modal fade" id="modalDetailAkselgate" tabindex="-1" role="dialog" aria-hidden="true">
     <div class="modal-dialog modal-xl" role="document">
         <div class="modal-content">
             <div class="modal-header">
-                <h5 class="modal-title">Detail Log Transaksi - <span id="akselgate-log-title">Kode Settle</span></h5>
+                <h5 class="modal-title">Detail Akselgate Response Log - <span id="akselgate-log-title">Kode Settle</span></h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                     <span aria-hidden="true">&times;</span>
                 </button>
